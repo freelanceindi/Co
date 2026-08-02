e701d815fd2bcff5a8882e9987d600555092b641
freelanceindi <subhashs1132@gmail.com>
2026-08-02 16:25:46 +0530

    Create dummy-data.js

---
+++ js/dummy-data.js
@@ -0,0 +1,97 @@
+const editors = [
+{
+id:1,
+name:"Aarav Mehta",
+city:"Mumbai",
+category:"YouTube",
+price:"₹2,500",
+rating:4.9,
+followers:"12.4K",
+experience:"5 Years",
+software:["Premiere Pro","After Effects"],
+portfolio:"portfolio.html?id=1",
+bio:"Cinematic YouTube & Brand Video Editor",
+thumbnail:"https://picsum.photos/400/500?random=1",
+avatar:"https://i.pravatar.cc/150?img=11"
+},
+
+{
+id:2,
+name:"Rohan Patil",
+city:"Pune",
+category:"Reels",
+price:"₹1,500",
+rating:4.8,
+followers:"8.1K",
+experience:"4 Years",
+software:["Premiere Pro"],
+portfolio:"portfolio.html?id=2",
+bio:"Instagram Reels Specialist",
+thumbnail:"https://picsum.photos/400/600?random=2",
+avatar:"https://i.pravatar.cc/150?img=12"
+},
+
+{
+id:3,
+name:"Sneha Sharma",
+city:"Delhi",
+category:"Wedding",
+price:"₹5,000",
+rating:5.0,
+followers:"20K",
+experience:"7 Years",
+software:["DaVinci Resolve"],
+portfolio:"portfolio.html?id=3",
+bio:"Luxury Wedding Film Editor",
+thumbnail:"https://picsum.photos/400/700?random=3",
+avatar:"https://i.pravatar.cc/150?img=13"
+},
+
+{
+id:4,
+name:"Aditya Verma",
+city:"Bangalore",
+category:"Commercial",
+price:"₹4,500",
+rating:4.9,
+followers:"15K",
+experience:"6 Years",
+software:["After Effects","Premiere Pro"],
+portfolio:"portfolio.html?id=4",
+bio:"Commercial & Brand Films",
+thumbnail:"https://picsum.photos/400/550?random=4",
+avatar:"https://i.pravatar.cc/150?img=14"
+},
+
+{
+id:5,
+name:"Neha Gupta",
+city:"Jaipur",
+category:"Motion",
+price:"₹3,800",
+rating:4.8,
+followers:"9K",
+experience:"5 Years",
+software:["After Effects"],
+portfolio:"portfolio.html?id=5",
+bio:"Motion Graphics Designer",
+thumbnail:"https://picsum.photos/400/650?random=5",
+avatar:"https://i.pravatar.cc/150?img=15"
+},
+
+{
+id:6,
+name:"Karan Singh",
+city:"Chandigarh",
+category:"Gaming",
+price:"₹2,200",
+rating:4.7,
+followers:"11K",
+experience:"3 Years",
+software:["Premiere Pro"],
+portfolio:"portfolio.html?id=6",
+bio:"Gaming Montage Editor",
+thumbnail:"https://picsum.photos/400/530?random=6",
+avatar:"https://i.pravatar.cc/150?img=16"
+}
+];
